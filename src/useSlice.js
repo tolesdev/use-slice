a0bf132b70aff6d3f9610376f90f95360eb84dbd
btoles <brennan.toles@gmail.com>
2019-05-12 22:11:35 -0500

    chore: Variable rename

--- src/useSlice.js
+++ src/useSlice.js
@@ -2,9 +2,9 @@ import React, { useReducer } from 'react';
 import createActions from './utils/createActions';
 import createReducer from './utils/createReducer';
 
-function useSlice(initialState = {}, actions) {
-    const actionCreators = createActions(actions);
-    const reducer = createReducer(actions);
+function useSlice(initialState = {}, reducers) {
+    const actionCreators = createActions(reducers);
+    const reducer = createReducer(reducers);
     const [state, dispatch] = useReducer(reducer, initialState);
     return [state, dispatch, actionCreators];
 }
